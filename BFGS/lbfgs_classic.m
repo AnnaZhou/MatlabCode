a6e58d9324125e0a37b00f6f1397d1642a87d7ea
Anna <zn.anna@gmail.com>
2015-02-25 12:04:25 -0500

    Update lbfgs_classic.m

--- BFGS/lbfgs_classic.m
+++ BFGS/lbfgs_classic.m
@@ -1,23 +1,26 @@
 
 function [w,iter,loss] = lbfgs_classic(x0,y0,fun)
  
- m = 30;
+ m = 50;
  k = 1;
- wDivision=1;
  eta0=0.1;
- eta=eta0;
  lr=0.01;
  gamma=0.1;
  delta=0.01;
  dw=1;
  batchSize=30;
  
- x=x0(:,1:m); y=y0(1,1:m);
  num =  size(x0,2); dim=size(x0,1);
  w0=zeros(dim,1); 
  w=w0+delta;
  g0=0;
  B=eye(dim)*delta;
+ 
+ x=x0(:,1); y=y0(1,1);
+ f= w'*x;
+ [v,g]=myloss(x,y,f,lr,w);
+ mys=w-w0;
+ myy=g-g0;
  
  for iter = 1:1000
      index=floor( rand(1,1)*(num-batchSize) );
@@ -27,27 +30,22 @@ function [w,iter,loss] = lbfgs_classic(x0,y0,fun)
      f= w'*x;
      [v,g]=myloss(x,y,f,lr,w);
 
-     s=w-w0;
-     yy=g-g0;
-     [p] = lbfgs(g,s,yy,B);
+     [p] = lbfgs(g,mys,myy,B);
      dw= norm(p);
-%      H=inv(B)+gamma*eye(dim);
      wt1=w-eta0*p;
      
      f= wt1'*x;
      [v1,g1]=myloss(x,y,f,lr,wt1);
      
-%      vt=wt1-w;
-%      rt=g1-g-delta*vt;
-%      
-%      B1=B*vt*vt'*B/(vt'*B*vt);
-%      if (vt'*rt~=0)
-%          B2=rt*rt'/(vt'*rt);
-%      else 
-%          B2=0;
-%      end
-%      B=B+B2-B1+delta*eye(dim);
- 
+     sk1=wt1-w;
+     yk1=g1-g;
+     mys=[mys,sk1];
+     myy=[myy,yk1];
+     if size(mys,2)>m
+         mys=mys(:,2:(m+1));
+         myy=myy(:,2:(m+1));
+     end
+     
      g0=g;
      w0=w;
      w=wt1;
