34f9051171c2b13b4ac5237e4309fc56ce08d9ba
Anna <zn.anna@gmail.com>
2015-03-06 11:41:09 -0500

    Calculate the gradient and sample loss based on lapSVM loss function

---
+++ LapSGD/mylossdual.m
@@ -0,0 +1,28 @@
+%%Calculate the gradient and sample loss based on lapSVM
+%%f = L1 loss + f_H + f_I
+
+function [v,g] = mylossdual(x,y,f1,f2,lr,w,K,L,options)
+  %x:dim*n; y:1*n; s:dim*1; v:1*1; g:1*n;
+  %w:1*n;
+  dim=size(x,1); n=size(x,2);
+  num=size(f1,2);
+  g= lr*w;
+  sumloss = zeros(1,n);   
+  v=0;
+  myk=calckernel(options,x',f1'); %1*n
+  
+  f=w'*myk'; 
+ 
+  l1=options.gamma_A*K*w;
+  l2=options.gamma_I*K*L*K*w;
+   
+
+   for i=1:num
+       d(1,i)=1-f(1,i)*f2(1,i);
+       v= v + d(1,i);
+       if d(1,i)>=0
+          sumloss(1,:)=sumloss(1,:) - f2(:,i)*myk(i,:);
+      end
+  end
+
+  g = l1 + (sumloss./num)' + l2;
